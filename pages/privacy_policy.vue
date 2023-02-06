009ec844a1584c5adc2898c2fe1232916f674aa3
Hiroki Tanaka <hiroki.tanaka@kumano-te.com>
2023-02-06 13:30:10 +0900

    feat: privacy policy page

---
+++ pages/privacy_policy.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="py-16 bg-white dark:bg-gray-900">
+    <div class="px-4 sm:px-6 lg:px-8 space-y-12">
+      <div class="prose mx-auto sm:prose-lg">
+        <h1>
+          <span
+            class="block text-sm text-center text-primary-600 font-semibold tracking-wide uppercase sm:text-base"
+            >Privacy policy</span
+          >
+          <span
+            class="mt-2 block text-2xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl dark:text-gray-50"
+            >個人情報保護方針</span
+          >
+        </h1>
+        <p class="mt-4 text-gray-700 sm:mt-8 dark:text-gray-200">
+          合同会社kumanote（以下「当社」）は、以下のとおり個人情報保護方針を定め、
+          個人情報保護の仕組みを構築し、全従業員に個人情報保護の重要性の認識と取組みを徹底させることにより、
+          個人情報の保護を推進致します。
+        </p>
+      </div>
+      <div class="prose prose-primary mx-auto sm:prose-lg">
+        <h3 class="text-gray-900 dark:text-gray-50">個人情報の管理</h3>
+        <p class="text-gray-700 dark:text-gray-200">
+          当社は、お客さまの個人情報を正確かつ最新の状態に保ち、
+          個人情報への不正アクセス・紛失・破損・改ざん・漏洩などを防止するため、
+          セキュリティシステムの維持・管理体制の整備・社員教育の徹底等の必要な措置を講じ、
+          安全対策を実施し個人情報の厳重な管理を行ないます。
+        </p>
+      </div>
+      <div class="prose prose-primary mx-auto sm:prose-lg">
+        <h3 class="text-gray-900 dark:text-gray-50">個人情報の利用目的</h3>
+        <p class="text-gray-700 dark:text-gray-200">
+          お客さまからお預かりした個人情報は、当社からのご連絡や業務のご案内やご質問に対する回答として、
+          電子メールや資料のご送付に利用いたします。
+        </p>
+      </div>
+      <div class="prose prose-primary mx-auto sm:prose-lg">
+        <h3 class="text-gray-900 dark:text-gray-50">
+          個人情報の第三者への開示・提供の禁止
+        </h3>
+        <p class="text-gray-700 dark:text-gray-200">
+          当社は、お客さまよりお預かりした個人情報を適切に管理し、次のいずれかに該当する場合を除き、
+          個人情報を第三者に開示いたしません。
+        </p>
+        <ol class="text-gray-700 dark:text-gray-200">
+          <li>お客さまの同意がある場合</li>
+          <li>
+            お客さまが希望されるサービスを行なうために当社が業務を委託する業者に対して開示する場合
+          </li>
+          <li>法令に基づき開示することが必要である場合</li>
+        </ol>
+      </div>
+      <div class="prose prose-primary mx-auto sm:prose-lg">
+        <h3 class="text-gray-900 dark:text-gray-50">個人情報の安全対策</h3>
+        <p class="text-gray-700 dark:text-gray-200">
+          当社は、個人情報の正確性及び安全性確保のために、セキュリティに万全の対策を講じています。
+        </p>
+      </div>
+      <div class="prose prose-primary mx-auto sm:prose-lg">
+        <h3 class="text-gray-900 dark:text-gray-50">ご本人の照会</h3>
+        <p class="text-gray-700 dark:text-gray-200">
+          お客さまがご本人の個人情報の照会・修正・削除などをご希望される場合には、
+          ご本人であることを確認の上、対応させていただきます。
+        </p>
+      </div>
+      <div class="prose prose-primary mx-auto sm:prose-lg">
+        <h3 class="text-gray-900 dark:text-gray-50">
+          法令、規範の遵守と見直し
+        </h3>
+        <p class="text-gray-700 dark:text-gray-200">
+          当社は、保有する個人情報に関して適用される日本の法令、
+          その他規範を遵守するとともに、本ポリシーの内容を適宜見直し、その改善に努めます。
+        </p>
+      </div>
+      <div class="prose prose-primary mx-auto sm:prose-lg">
+        <h3 class="text-gray-900 dark:text-gray-50">
+          Cookie及びこれに類する技術の利用
+        </h3>
+        <p class="text-gray-700 dark:text-gray-200">
+          当社のサービスは、Cookie及びこれに類する技術を利用することがあります。
+          これらの技術を使って収集したデータは、当社サービスの利便性向上のために使用されます。
+          お使いのブラウザの設定から、Cookieを無効化することができますが、
+          その場合、当社サービスの一部の機能をご利用いただけなる場合があります。
+        </p>
+      </div>
+      <div class="prose prose-primary mx-auto sm:prose-lg">
+        <h3 class="text-gray-900 dark:text-gray-50">
+          アクセス解析ツール・クラッシュレポートについて
+        </h3>
+        <p class="text-gray-700 dark:text-gray-200">
+          当社のサービスは、当社のサービス利用動向の調査や最適な広告配信をする為に、
+          第三者企業によるクッキー、画像ファイル（ウェブビーコン）などを用い、トラフィックデータの収集を行っています。
+          また弊社が提供するモバイルアプリケーションにはアプリケーション動作中に動作が停止（クラッシュ）した場合に、
+          その原因を解析するためのクラッシュレポートなどを収集する場合があります。
+          これらの中には、第三者企業が直接情報を取得するものも含まれ、
+          そこで収集されるデータは第三者企業が定めるプライバシーポリシーにしたがって管理されます。
+          当社サービスが使用しているアクセス解析ツールに関する第三者企業のプライバシーポリシーやクッキーに関する説明については、
+          以下をご覧ください。
+        </p>
+        <div class="text-gray-700 dark:text-gray-200">
+          Google LLC
+          <ul>
+            <li>
+              <a
+                target="_blank"
+                href="https://policies.google.com/privacy?hl=ja"
+                class="text-primary-500 hover:text-primary-400 underline"
+                >プライバシー ポリシー – ポリシーと規約 – Google</a
+              >
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://firebase.google.com/support/privacy?hl=ja"
+                class="text-primary-500 hover:text-primary-400 underline"
+                >Firebase のプライバシーとセキュリティ</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
