0834ebb5d44fc3ce5dc9c8de93a458c7e8088934
Hiroki Tanaka <hiroki.tanaka@kumano-te.com>
2023-02-13 13:57:07 +0900

    style: fix desktop global header font size

--- components/headers/AppHeader.vue
+++ components/headers/AppHeader.vue
@@ -132,7 +132,7 @@
             <!-- Desktop Navigation -->
             <nav class="pointer-events-auto hidden md:block">
               <ul
-                class="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
+                class="flex rounded-full bg-white/90 px-3 text-xs font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 lg:text-sm"
               >
                 <li v-for="item in navItems" :key="item.href">
                   <NuxtLink
