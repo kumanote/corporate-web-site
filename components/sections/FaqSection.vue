996c71d7d3908d0abb5387a45234f030d3ac07ed
Hiroki Tanaka <hiroki.tanaka@kumano-te.com>
2023-02-14 12:58:06 +0900

    fix: faq font color

--- components/sections/FaqSection.vue
+++ components/sections/FaqSection.vue
@@ -10,23 +10,23 @@
       <div class="lg:grid lg:grid-cols-12 lg:gap-8">
         <div class="lg:col-span-5">
           <h2
-            class="text-2xl font-bold leading-10 tracking-tight text-gray-900"
+            class="text-2xl font-bold leading-10 tracking-tight text-gray-900 dark:text-gray-50"
           >
             {{ $t('faq_title') }}
           </h2>
-          <p class="mt-4 text-base leading-7 text-gray-600">
+          <p class="mt-4 text-base leading-7 text-gray-600 dark:text-gray-400">
             <template v-if="locale === 'en'">
               For official company information, please visit
               <NuxtLink
                 :href="localePath('/about')"
-                class="text-primary-600 hover:text-primary-500 underline font-semibold"
+                class="text-primary-600 hover:text-primary-500 underline font-semibold dark:text-primary-500 dark:hover:text-primary-600"
                 >here</NuxtLink
               >.
             </template>
             <template v-else>
               会社情報は<NuxtLink
                 :href="localePath('/about')"
-                class="text-primary-600 hover:text-primary-500 underline font-semibold"
+                class="text-primary-600 hover:text-primary-500 underline font-semibold dark:text-primary-500 dark:hover:text-primary-600"
                 >こちら</NuxtLink
               >をご覧ください。
             </template>
@@ -35,10 +35,14 @@
         <div class="mt-10 lg:col-span-7 lg:mt-0">
           <dl class="space-y-10">
             <div v-for="item in faqs" :key="item">
-              <dt class="text-base font-semibold leading-7 text-gray-900">
+              <dt
+                class="text-base font-semibold leading-7 text-gray-900 dark:text-gray-50"
+              >
                 {{ $t(`faq.${item}.question`) }}
               </dt>
-              <dd class="mt-2 text-base leading-7 text-gray-600 break-words">
+              <dd
+                class="mt-2 text-base leading-7 text-gray-600 break-words dark:text-gray-400"
+              >
                 {{ $t(`faq.${item}.answer`) }}
               </dd>
             </div>
